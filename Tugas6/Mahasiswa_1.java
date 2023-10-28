f605f326928c0e0e7d56b6ef295b3ea23bee27fc
agustinalatifatulm <149041312+agustinalatifatulm@users.noreply.github.com>
2023-10-28 10:53:47 +0700

    Add files via upload

---
+++ Tugas6/Mahasiswa_1.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cobaDB;
+
+/**
+ *
+ * @author HP
+ */
+public class Mahasiswa_1 {
+    
+    private String NIM;
+    private String Nama;
+    private String Alamat;
+
+    /**
+     * @return the NIM
+     */
+    public String getNIM() {
+        return NIM;
+    }
+
+    /**
+     * @param NIM the NIM to set
+     */
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    /**
+     * @return the Nama
+     */
+    public String getNama() {
+        return Nama;
+    }
+
+    /**
+     * @param Nama the Nama to set
+     */
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    /**
+     * @return the Alamat
+     */
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    /**
+     * @param Alamat the Alamat to set
+     */
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+}
+   
