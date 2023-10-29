8113a24dbab20a88ea9e3fd53590eddef6a6a8eb
agustinalatifatulm <149041312+agustinalatifatulm@users.noreply.github.com>
2023-10-29 09:15:41 +0700

    Add files via upload

---
+++ Tugas9/bukuPOJO.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CRUD;
+
+/**
+ *
+ * @author HP
+ */
+public class bukuPOJO {
+    private String  Isbn;
+    private String judul;
+    private String tahunterbit;
+    private String penerbit;
+
+    /**
+     * @return the isbn
+     */
+    public String getISBN() {
+        return  Isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this. Isbn = isbn;
+    }
+
+    /**
+     * @return the judul
+     */
+    public String getJudul() {
+        return judul;
+    }
+
+    /**
+     * @param judul the judul to set
+     */
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    /**
+     * @return the tahunterbit
+     */
+    public String getTahunterbit() {
+        return tahunterbit;
+    }
+
+    /**
+     * @param tahunterbit the tahunterbit to set
+     */
+    public void setTahunterbit(String tahunterbit) {
+        this.tahunterbit = tahunterbit;
+    }
+
+    /**
+     * @return the penerbit
+     */
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    /**
+     * @param penerbit the penerbit to set
+     */
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+}
